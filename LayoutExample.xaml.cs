2731839d551dc61f51abc124d34cceb200b25039
Walker Crystal <129323638+indelible-seven@users.noreply.github.com>
2023-06-19 03:02:11 -0600

    inline platform specific formatting

--- LayoutExample.xaml.cs
+++ LayoutExample.xaml.cs
@@ -5,5 +5,8 @@ public partial class LayoutExample : ContentPage
 	public LayoutExample()
 	{
 		InitializeComponent();
+
+		// Set padding for the StackLayout based on the device platform
+		VStackLayout.Padding = DeviceInfo.Platform == DevicePlatform.iOS ? new Thickness(30, 10, 30, 0) : new Thickness(25);
 	}
 }
