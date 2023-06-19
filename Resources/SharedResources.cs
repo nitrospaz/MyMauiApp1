f15f0c794eadcb08aca5d8b599e8743a318f6dd4
Walker Crystal <129323638+indelible-seven@users.noreply.github.com>
2023-06-19 04:12:14 -0600

    implementing shared resource file for fonts, colors, ect.

---
+++ Resources/SharedResources.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1.Resources
+{
+	static class SharedResources
+	{
+		public static readonly Color FontColour = Color.FromRgb(0, 255, 0);
+
+		public static readonly Color BackgroundColour = Color.FromRgb(255, 255, 255);
+	}
+}
